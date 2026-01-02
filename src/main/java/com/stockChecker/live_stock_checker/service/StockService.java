8aad95c53d3a31e84e5513a47ab96634544124a6
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-01-02 17:41:47 +0530

    Implement live stock data fetching with lazy initialization and metadata caching

--- src/main/java/com/stockChecker/live_stock_checker/service/StockService.java
+++ src/main/java/com/stockChecker/live_stock_checker/service/StockService.java
@@ -1,6 +1,7 @@
 package com.stockChecker.live_stock_checker.service;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.stockChecker.live_stock_checker.payload.StockDetailResponseDTO;
 import com.stockChecker.live_stock_checker.payload.StockResponse;
 import com.stockChecker.live_stock_checker.payload.StockSearchResponseDTO;
@@ -9,7 +10,7 @@ import org.springframework.data.domain.Page;
 import java.util.List;
 
 public interface StockService {
-    StockDetailResponseDTO getStockBySymbol(String symbol);
+    StockDetailResponseDTO getStockBySymbol(String symbol) throws JsonProcessingException;
 
     StockResponse searchStockByName(String query, Integer pageNumber, Integer pageSize, String sortBy, String sortOrder);
 }
--- src/main/java/com/stockChecker/live_stock_checker/service/StockServiceImpl.java
+++ src/main/java/com/stockChecker/live_stock_checker/service/StockServiceImpl.java
@@ -1,12 +1,12 @@
 package com.stockChecker.live_stock_checker.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stockChecker.live_stock_checker.exceptions.StockNotFoundException;
 import com.stockChecker.live_stock_checker.model.Company;
 import com.stockChecker.live_stock_checker.model.Stock;
-import com.stockChecker.live_stock_checker.payload.CompanyResponseDTO;
-import com.stockChecker.live_stock_checker.payload.StockDetailResponseDTO;
-import com.stockChecker.live_stock_checker.payload.StockResponse;
-import com.stockChecker.live_stock_checker.payload.StockSearchResponseDTO;
+import com.stockChecker.live_stock_checker.payload.*;
 import com.stockChecker.live_stock_checker.repository.CompanyRepository;
 import com.stockChecker.live_stock_checker.repository.StockRepository;
 import org.modelmapper.ModelMapper;
@@ -16,7 +16,9 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 
@@ -31,42 +33,115 @@ public class StockServiceImpl implements StockService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private WebClient webClient;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
     /*
         entire flow, first checking if the stock exits in database,
         if not then we call to save into the Database, and then it returns that stock back.
     */
 
     @Override
-    public StockDetailResponseDTO getStockBySymbol(String symbol) {
-        Stock stockFound = stockRepository.findByStockSymbol(symbol)
-                .orElseThrow(() ->
-                        new StockNotFoundException("Stock not found with symbol: " + symbol)
-                );
+    public StockDetailResponseDTO getStockBySymbol(String symbol) throws JsonProcessingException {
+        /*
+            checking if the stock is valid or not as both invalid and valid response's have 200 status code.
+            Now,to fix this we would manually check if error field exists->throw Exception
+
+            Otherwise,if the "error" field doesn't exist then it's a valid symbol, and we got a valid
+            realTime stock response.
+         */
+
+        String jsonResponseString = webClient.get()
+                .uri("/quote-equity?symbol=" + symbol.toUpperCase())
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+
+        JsonNode rootNode = objectMapper.readTree(jsonResponseString);
+
+        if (rootNode.has("error") || rootNode.has("message")) {
+            throw new StockNotFoundException("This stock doesnt exists with symbol: " + symbol);
+        }
+
+        /*
+            Understanding Mono<T> in SpringBoot
+
+            So whenever you use a webClient, the output of it is always Mono,which can have different values like
+            Mono<String> or Mono<ResponseEntity<String>> so on.
+
+            understand mono to be a container, since webclient is reactive in nature, allowing non-blocking asynchronous
+            calling/execution of requests so the value is empty first which is mono but later on when the response is
+            received, it has to be assigned somewhere again so it get's filled in that mono container which was assigned
+            to our variable.
+
+            Initially, Mono is assigned until the response is received back and as we get so it gets filled by the response
+            basically a container holding/reserving a place for the response to be kept in later when its received.
+        */
+
+        JsonNode infoNode = rootNode.get("info");
+        JsonNode priceInfoNode = rootNode.get("priceInfo");
+
+        Stock stockFound = stockRepository.findByStockSymbol(symbol.toUpperCase())
+                .orElseGet(() -> saveStockInDB(symbol, rootNode, infoNode));
+        // Lazy Initialization - object creation delayed until first access
+
         StockDetailResponseDTO stockFoundDTO = modelMapper.map(stockFound, StockDetailResponseDTO.class);
         // companyDTO remains empty so need to fix and fill that.
         if (stockFound.getCompany() != null) {
             CompanyResponseDTO companyResponseDTO = modelMapper.map(stockFound.getCompany(), CompanyResponseDTO.class);
             stockFoundDTO.setCompanyResponseDTO(companyResponseDTO);
         }
+        // creating a PriceInfoDTO
+        PriceInfoDTO priceInfoDTO = new PriceInfoDTO();
+        priceInfoDTO.setLastPrice(new BigDecimal(priceInfoNode.get("lastPrice").asText()));
+        priceInfoDTO.setChange(new BigDecimal(priceInfoNode.get("change").asText()));
+        priceInfoDTO.setPChange(new BigDecimal(priceInfoNode.get("pChange").asText()));
+        priceInfoDTO.setPreviousClose(new BigDecimal(priceInfoNode.get("previousClose").asText()));
+        priceInfoDTO.setOpen(new BigDecimal(priceInfoNode.get("open").asText()));
+        priceInfoDTO.setClose(new BigDecimal(priceInfoNode.get("close").asText()));
+        // setting the high-low of the day
+        priceInfoDTO.setDayHigh(new BigDecimal(priceInfoNode.get("intraDayHighLow").get("max").asText()));
+        priceInfoDTO.setDayLow(new BigDecimal(priceInfoNode.get("intraDayHighLow").get("min").asText()));
+
+        // setting the high-low of the week
+        priceInfoDTO.setWeekLow(new BigDecimal(priceInfoNode.get("weekHighLow").get("min").asText()));
+        priceInfoDTO.setWeekLowDate(priceInfoNode.get("weekHighLow").get("minDate").asText());
+        priceInfoDTO.setWeekHigh(new BigDecimal(priceInfoNode.get("weekHighLow").get("max").asText()));
+        priceInfoDTO.setWeekHighDate(priceInfoNode.get("weekHighLow").get("maxDate").asText());
+
+        // setting the lowerCircuitPrice and upperCircuitPrice
+        priceInfoDTO.setLowerCP(new BigDecimal(priceInfoNode.get("lowerCP").asText()));
+        priceInfoDTO.setUpperCP(new BigDecimal(priceInfoNode.get("upperCP").asText()));
+        priceInfoDTO.setBasePrice(new BigDecimal(priceInfoNode.get("basePrice").asText()));
+
+        stockFoundDTO.setPriceInfoDTO(priceInfoDTO);
+//      Combined stored metadata with real-time price data and returning complete StockFoundDTO
         return stockFoundDTO;
     }
 
     // private method which is being used in implementation and not allowed outside
-    private Stock saveStockInDB(String symbol) {
+    private Stock saveStockInDB(String symbol, JsonNode rootNode, JsonNode infoNode) {
 
         Stock newStock = Stock.builder()
-                .stockName("ABC")
-                .stockWebsite("abc.com")
+                .stockName(infoNode.get("companyName").asText().toUpperCase())
+                .stockWebsite(null)
                 .listedExchangeName("NSE")
-                .stockSymbol(symbol)
+                .stockSymbol(infoNode.get("symbol").asText())
                 .build();
         // as stock gets created we continue the creation of its company alongside as they are OneToOne mapped
         Company newCompany = Company.builder()
-                .companyName(symbol + " company")
-                .aboutCompany("this company works produces aircrafts")
-                .sector("Industries")
-                .industry("aerospace")
-                .foundedYear(1980)
+                .companyName(infoNode.get("companyName").asText())
+                .aboutCompany("this company works is " + infoNode.get("companyName").asText())// update with yahoo api
+                .sector(rootNode.get("industryInfo").get("sector").asText())
+                .subIndustry(infoNode.get("industry").asText())
+                .industry(rootNode.get("industryInfo").get("industry").asText())
+                .listingDate(rootNode.get("metadata").get("listingDate").asText())
+                .isIN(rootNode.get("metadata").get("isin").asText())
                 .stock(newStock)
                 .build();
         companyRepository.save(newCompany);
