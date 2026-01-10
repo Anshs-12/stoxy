983fc45e6f1464d8762c8a0248920b6d8b9e7e06
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-01-11 01:24:49 +0530

    added IndexController with seachByIndexSymbol endpoint

--- src/main/java/com/stockChecker/live_stock_checker/controller/IndexController.java
+++ src/main/java/com/stockChecker/live_stock_checker/controller/IndexController.java
@@ -1,4 +1,28 @@
 package com.stockChecker.live_stock_checker.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.stockChecker.live_stock_checker.payload.IndexPayload.IndexDetailResponseDTO;
+import com.stockChecker.live_stock_checker.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/index")
 public class IndexController {
+
+    @Autowired
+    private IndexService indexService;
+
+    // get entire Index Response by indexSymbol
+    @GetMapping("/search/{indexSymbol}")
+    public ResponseEntity<IndexDetailResponseDTO> getIndexBySymbol(@PathVariable String indexSymbol) throws JsonProcessingException {
+        IndexDetailResponseDTO indexDetailResponseDTO = indexService.getIndexBySymbol(indexSymbol);
+        return new ResponseEntity<>(indexDetailResponseDTO, HttpStatus.OK);
+    }
+
 }
