f2e380e4926f1ceaca483c26fbd156ea4e55f4c3
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-01-14 02:15:36 +0530

    created CacheConfig File and added @EnableCaching annotation

---
+++ src/main/java/com/stockChecker/live_stock_checker/config/CacheConfig.java
@@ -0,0 +1,9 @@
+package com.stockChecker.live_stock_checker.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {
+}
