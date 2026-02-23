4b38a7b5d8900eaa58cd8bf78d351f3e5a956ce5
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-02-24 01:26:08 +0530

    feat: extend StockRepository with JpaSpecificationExecutor for dynamic filtering

--- src/main/java/com/stockChecker/live_stock_checker/repository/StockRepository.java
+++ src/main/java/com/stockChecker/live_stock_checker/repository/StockRepository.java
@@ -4,15 +4,15 @@ import com.stockChecker.live_stock_checker.model.Stock;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface StockRepository extends JpaRepository<Stock, Integer> {
+public interface StockRepository extends JpaRepository<Stock, Integer>, JpaSpecificationExecutor<Stock> {
 
-//    @Query(" SELECT * FROM stock WHERE stock_name=?1 LIKE '%ta%' ORDER BY CASE WHEN stock_name=?2 I LIKE 'ta%'THEN 1 ELSE 2 END, stock_name LIMIT 15 OFFSET 0;")
+    //    @Query(" SELECT * FROM stock WHERE stock_name=?1 LIKE '%ta%' ORDER BY CASE WHEN stock_name=?2 I LIKE 'ta%'THEN 1 ELSE 2 END, stock_name LIMIT 15 OFFSET 0;")
     Page<Stock> findByStockNameContainingIgnoreCaseOrStockSymbolContainingIgnoreCase(String query1, String query2, Pageable pageable);
 
     Optional<Stock> findByStockSymbol(String symbol);
