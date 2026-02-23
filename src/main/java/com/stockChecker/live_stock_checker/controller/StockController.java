17c69998a0006aeff381389f7983848c66507a6a
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-02-24 01:25:51 +0530

    feat: add stock screening endpoint with dynamic filters, sorting and pagination

--- src/main/java/com/stockChecker/live_stock_checker/controller/StockController.java
+++ src/main/java/com/stockChecker/live_stock_checker/controller/StockController.java
@@ -3,6 +3,8 @@ package com.stockChecker.live_stock_checker.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.stockChecker.live_stock_checker.payload.StockPayload.StockDetailResponseDTO;
 import com.stockChecker.live_stock_checker.payload.StockPayload.StockResponse;
+import com.stockChecker.live_stock_checker.payload.StockPayload.StockScreenerDTO;
+import com.stockChecker.live_stock_checker.payload.StockPayload.StockScreenerResponseDTO;
 import com.stockChecker.live_stock_checker.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -30,10 +32,29 @@ public class StockController {
         return new ResponseEntity<>(stockListDTO, HttpStatus.OK);
     }
 
-    // get entire stock object
+    // get the entire stock object
     @GetMapping("/search/details/{stockSymbol}")
     public ResponseEntity<StockDetailResponseDTO> searchStockBySymbol(@PathVariable String stockSymbol) throws JsonProcessingException {
         StockDetailResponseDTO stockDetailDTO = stockService.getStockBySymbol(stockSymbol);
         return new ResponseEntity<>(stockDetailDTO, HttpStatus.OK);
+    }
+
+    // screen
+    @GetMapping("/search/screen")
+    public ResponseEntity<StockScreenerDTO> searchScreenStocks(
+            @RequestParam(required = false) Double minPe,
+            @RequestParam(required = false) Double maxPe,
+            @RequestParam(required = false) String sector,
+            @RequestParam(required = false) String industry,
+
+            @RequestParam(name = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
+            @RequestParam(name = "pageSize", defaultValue = "15", required = false) Integer pageSize,
+            @RequestParam(name = "sortBy", defaultValue = "stockName", required = false) String sortBy,
+            @RequestParam(name = "sortOrder", defaultValue = "asc", required = false) String sortOrder
+    ) {
+
+        StockScreenerDTO StockScreenerResponseDTO = stockService
+                .searchScreenStocks(minPe, maxPe, sector, industry, pageNumber, pageSize, sortBy, sortOrder);
+        return new ResponseEntity<>(StockScreenerResponseDTO, HttpStatus.OK);
     }
 }
