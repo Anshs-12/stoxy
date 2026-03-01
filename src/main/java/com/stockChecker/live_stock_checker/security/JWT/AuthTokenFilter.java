7c9bf0ee9d6140d2e0e9793601cfcb2000602391
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-03-01 20:46:17 +0530

    feat: add AuthTokenFilter to validate JWT from header and cookie on every request

---
+++ src/main/java/com/stockChecker/live_stock_checker/security/JWT/AuthTokenFilter.java
@@ -0,0 +1,52 @@
+package com.stockChecker.live_stock_checker.security.JWT;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        try {
+            String jwtToken = jwtUtils.getJwtFromHeader(request);
+            if (jwtToken == null) {
+                jwtToken = jwtUtils.getJwtFromCookie(request);
+            }
+            if (jwtToken != null && jwtUtils.validateJwtToken(jwtToken)) {
+                // this helps in maintaining proper checking across as we are
+                // going to set the SecurityContext here,so maintaining all the checks
+
+                String email = jwtUtils.getEmailFromToken(jwtToken);
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                        new UsernamePasswordAuthenticationToken(email, null, List.of());
+                usernamePasswordAuthenticationToken.setDetails(
+                        new WebAuthenticationDetailsSource()
+                                .buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            }
+        } catch (Exception ex) {
+            log.error("Cannot set user authentication: {}", ex.getMessage());
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
