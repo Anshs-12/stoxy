d804363f342629fa123753322f7b5a93c8d21c94
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-03-01 21:40:40 +0530

    feat: Add /logout endpoint to clear JWT cookie

--- src/main/java/com/stockChecker/live_stock_checker/controller/AuthController.java
+++ src/main/java/com/stockChecker/live_stock_checker/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.stockChecker.live_stock_checker.controller;
 
 import com.stockChecker.live_stock_checker.payload.UserInfoResponseDTO;
+import com.stockChecker.live_stock_checker.security.JWT.JwtUtils;
 import com.stockChecker.live_stock_checker.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,13 +18,27 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    /*
+        /oauth2/authorization/google → this IS the login, handled by Spring automatically
+        /oauth2/user → returns user info after login
+        /oauth2/logout → clears token
+    */
     private final AuthService authService;
+    private final JwtUtils jwtUtils;
 
-    @GetMapping("/login")
-    public ResponseEntity<UserInfoResponseDTO> login() {
+    @GetMapping("/user")
+    public ResponseEntity<UserInfoResponseDTO> userInfo() {
         String userEmail =
                 SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
         UserInfoResponseDTO userInfoResponseDTO = authService.getUserInfo(userEmail);
         return new ResponseEntity<>(userInfoResponseDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<String> logout() {
+        ResponseCookie cleanCookie = jwtUtils.generateCleanJwtCookie();
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cleanCookie.toString())
+                .body("User has been logout!");
     }
 }
