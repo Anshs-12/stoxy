157520661367c1aebdd0d05185a8ebe3f5ac8250
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-03-01 20:46:52 +0530

    feat: add User entity with userId, name, email and authProvider fields

---
+++ src/main/java/com/stockChecker/live_stock_checker/model/User.java
@@ -0,0 +1,32 @@
+package com.stockChecker.live_stock_checker.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId", unique = true, nullable = false)
+    private Long userId;
+
+    @Column(name = "name", unique = false, nullable = false)
+    private String name;
+
+    @Column(name = "userMailId", unique = true, nullable = false)
+    private String userMailId;
+
+    @Column(name = "authProvider", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private OAuth2Provider authProvider;
+
+}
