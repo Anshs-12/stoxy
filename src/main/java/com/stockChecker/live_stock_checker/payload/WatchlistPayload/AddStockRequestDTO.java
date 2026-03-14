c0076d70cd6ae21bf308f3e16ecef9f00b23f7fe
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-03-15 02:51:56 +0530

    feat: add AddStockRequestDTO for addStockToWatchlist endpoint

---
+++ src/main/java/com/stockChecker/live_stock_checker/payload/WatchlistPayload/AddStockRequestDTO.java
@@ -0,0 +1,13 @@
+package com.stockChecker.live_stock_checker.payload.WatchlistPayload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class AddStockRequestDTO {
+    private String stockSymbol;
+}
