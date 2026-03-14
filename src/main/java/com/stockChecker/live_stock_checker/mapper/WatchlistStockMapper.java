076aa51fc7d87e575fdb825251e99cbbae995d75
anshs-12 <144445572+Anshs-12@users.noreply.github.com>
2026-03-15 02:42:43 +0530

    feat: add WatchlistStockMapper for WatchlistStock to DTO mapping

---
+++ src/main/java/com/stockChecker/live_stock_checker/mapper/WatchlistStockMapper.java
@@ -0,0 +1,14 @@
+package com.stockChecker.live_stock_checker.mapper;
+
+import com.stockChecker.live_stock_checker.model.WatchlistStock;
+import com.stockChecker.live_stock_checker.payload.WatchlistPayload.WatchlistStockResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface WatchlistStockMapper {
+
+    @Mapping(source = "stock.stockName", target = "stockName")
+    @Mapping(source = "stock.stockSymbol", target = "stockSymbol")
+    WatchlistStockResponseDTO toWatchlistStockResponseDTO(WatchlistStock watchlistStock);
+}
