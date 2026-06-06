6f02f94a82a478c08a7959c5d831d43e8391b273
anshs-12 <anshsugandhi12@gmail.com>
2026-06-06 17:50:51 +0530

    feat: add UpstoxFeedException for Upstox API error handling

---
+++ src/main/java/com/stockChecker/live_stock_checker/exceptions/UpstoxFeedException.java
@@ -0,0 +1,7 @@
+package com.stockChecker.live_stock_checker.exceptions;
+
+public class UpstoxFeedException extends RuntimeException {
+    public UpstoxFeedException(String message) {
+        super(message);
+    }
+}
