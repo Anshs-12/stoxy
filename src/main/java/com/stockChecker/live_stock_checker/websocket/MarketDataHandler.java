65c11ba6af81b57e0a2a1fd16daa00f1c6cbe16b
anshs-12 <anshsugandhi12@gmail.com>
2026-06-12 04:28:47 +0530

    feat: integrate Upstox WebSocket pipeline with Protobuf binary parsing and Redis tick caching

--- src/main/java/com/stockChecker/live_stock_checker/websocket/MarketDataHandler.java
+++ src/main/java/com/stockChecker/live_stock_checker/websocket/MarketDataHandler.java
@@ -1,59 +1,130 @@
 package com.stockChecker.live_stock_checker.websocket;
 
-import com.stockChecker.live_stock_checker.payload.UpstoxPayload.UpstoxSubscribeRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.protobuf.InvalidProtocolBufferException;
+import com.stockChecker.live_stock_checker.payload.WebsocketPayload.FullFeedDataDTO;
+import com.stockChecker.live_stock_checker.payload.WebsocketPayload.LtpcDataDTO;
+import com.stockChecker.live_stock_checker.payload.WebsocketPayload.QuoteDTO;
+import com.stockChecker.live_stock_checker.service.IndexService;
+import com.upstox.marketdatafeederv3udapi.rpc.proto.MarketDataFeedV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
 import java.net.http.WebSocket;
 import java.nio.ByteBuffer;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
+@RequiredArgsConstructor
+@Component
+@Slf4j
 public class MarketDataHandler implements WebSocket.Listener {
+
+    private final IndexService indexService;
+    private final ObjectMapper objectMapper;
+    private final WebSocketManager webSocketManager;
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
     @Override
     public void onOpen(WebSocket webSocket) {
         webSocket.request(1);
-        // upstox subscribe logic
-        webSocket.sendText(new UpstoxSubscribeRequest().toString(), true);
     }
 
     @Override
     public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+        if (statusCode == 1000) {
+            log.info("WebSocket closed gracefully. Reason: {}", reason);
+        } else {
+            log.warn("Websocket connection killed violently! Code: {}", statusCode);
+        }
+        return null;
+    }
 
+    @Override
+    public void onError(WebSocket webSocket, Throwable error) {
+        log.error("FATAL: WebSocket network layer failure. Reason: {}", error.getMessage(), error);
     }
 
     @Override
     public CompletionStage<?> onBinary(WebSocket webSocket,
                                        ByteBuffer data,
                                        boolean last) {
+        int remainingBytes = data.remaining();
+        byte[] payloadBytes = new byte[remainingBytes];
+        data.get(payloadBytes); // scooping out entire data
+
+        try {
+            MarketDataFeedV3.FeedResponse response = MarketDataFeedV3.FeedResponse.parseFrom(payloadBytes);
+            if (response.getType() == MarketDataFeedV3.Type.live_feed) {
+                Map<String, MarketDataFeedV3.Feed> responseFeedsMap = response.getFeedsMap();
+                for (String instrumentKey : responseFeedsMap.keySet()) {
+                    MarketDataFeedV3.Feed stockData = responseFeedsMap.get(instrumentKey);
+                    if (stockData.hasLtpc()) {
+                        LtpcDataDTO ltpcPayload = generateLtpcDTO(instrumentKey, stockData.getLtpc());
+                        redisTemplate.opsForValue().set("LTPC:" + instrumentKey, ltpcPayload, Duration.ofMinutes(3));
+                    } else if (stockData.hasFullFeed()) {
+                        LtpcDataDTO ltpcPayload = generateLtpcDTO(instrumentKey, stockData.getFullFeed().getMarketFF().getLtpc());
+                        redisTemplate.opsForValue().set("LTPC:" + instrumentKey, ltpcPayload, Duration.ofMinutes(3));
+
+                        FullFeedDataDTO fullFeedPayload = generateFullFeedDTO(instrumentKey, stockData);
+                        redisTemplate.opsForValue().set("FULL:" + instrumentKey, fullFeedPayload, Duration.ofMinutes(3));
+                    }
+                }
+            }
+            webSocket.request(1);
+        } catch (InvalidProtocolBufferException e) {
+            // Throwing error is avoided, Request the next frame to keep the stream alive.
+            webSocket.request(1);
+        }
         return null;
     }
 
-    @Override
-    public void onError(WebSocket webSocket, Throwable error) {
-
+    private LtpcDataDTO generateLtpcDTO(String instrumentKey, MarketDataFeedV3.LTPC rawLtpcData) {
+        return LtpcDataDTO.builder().instrumentKey(instrumentKey)
+                .lastTradedPrice(BigDecimal.valueOf(rawLtpcData.getLtp()))
+                .lastTradedTime((rawLtpcData.getLtt()))
+                .closePrice(BigDecimal.valueOf(rawLtpcData.getCp()))
+                .build();
     }
 
-    private List<String> getAllMajorIndices() {
-        return List.of("NSE_INDEX|Nifty 50",
-                "NSE_INDEX|Nifty Bank",
-                "NSE_INDEX|Nifty IT",
-                "NSE_INDEX|Nifty Midcap 100",
-                "NSE_INDEX|Nifty Next 50",
-                "NSE_INDEX|Nifty FMCG",
-                "NSE_INDEX|Nifty Auto",
-                "NSE_INDEX|Nifty Pharma",
-                "NSE_INDEX|Nifty Realty",
-                "NSE_INDEX|Nifty Metal",
-                "NSE_INDEX|Nifty Energy",
-                "NSE_INDEX|Nifty Infrastructure",
-                "NSE_INDEX|Nifty PSE",
-                "NSE_INDEX|Nifty Commodities",
-                "NSE_INDEX|Nifty Media",
-                "NSE_INDEX|Nifty Private Bank",
-                "NSE_INDEX|Nifty Financial Services",
-                "BSE_INDEX|Sensex",
-                "BSE_INDEX|Sensex 50",
-                "BSE_INDEX|BSE 10",
-                "GLOBAL_INDEX|SGX NIFTY");
+    private FullFeedDataDTO generateFullFeedDTO(String instrumentKey, MarketDataFeedV3.Feed stockData) {
+        return FullFeedDataDTO.builder()
+                .instrumentKey(instrumentKey)
+                .lastTradedPrice(BigDecimal.valueOf(stockData.getFullFeed().getMarketFF().getLtpc().getLtp()))
+                .lastTradedTime(stockData.getFullFeed().getMarketFF().getLtpc().getLtt())
+                .closePrice(BigDecimal.valueOf(stockData.getFullFeed().getMarketFF().getLtpc().getCp()))
+                .marketLevel(getMarketLevelQuoteList(stockData.getFullFeed().getMarketFF().getMarketLevel()))
+                .averageTradedPrice(BigDecimal.valueOf(stockData.getFullFeed().getMarketFF().getAtp()))
+                .volumeTradedToday(stockData.getFullFeed().getMarketFF().getVtt())
+                .openInterest(BigDecimal.valueOf(stockData.getFullFeed().getMarketFF().getOi()))
+                .impliedVolatility(BigDecimal.valueOf(stockData.getFullFeed().getMarketFF().getIv()))
+                .totalBuyQuantity((long) stockData.getFullFeed().getMarketFF().getTbq())
+                .totalSellQuantity((long) stockData.getFullFeed().getMarketFF().getTsq())
+                .build();
+    }
+
+    private List<QuoteDTO> getMarketLevelQuoteList(MarketDataFeedV3.MarketLevel marketLevel) {
+        List<QuoteDTO> responseList = new ArrayList<>();
+        for (var eachItem : marketLevel.getBidAskQuoteList()) {
+            QuoteDTO eachQuoteDTO = QuoteDTO.builder()
+                    .bidQuantity(eachItem.getBidQ())
+                    .bidPrice(BigDecimal.valueOf(eachItem.getBidP()))
+                    .askQuantity(eachItem.getAskQ())
+                    .askPrice(BigDecimal.valueOf(eachItem.getAskP()))
+                    .build();
+            responseList.add(eachQuoteDTO);
+        }
+        return responseList;
     }
 }
--- src/main/java/com/stockChecker/live_stock_checker/websocket/UpstoxWebSocketClient.java
+++ src/main/java/com/stockChecker/live_stock_checker/websocket/UpstoxWebSocketClient.java
@@ -4,6 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stockChecker.live_stock_checker.exceptions.UpstoxFeedException;
+import com.stockChecker.live_stock_checker.payload.StockPayload.StockSearchDTO;
+import com.stockChecker.live_stock_checker.payload.UpstoxPayload.UpstoxSubscribeData;
+import com.stockChecker.live_stock_checker.payload.UpstoxPayload.UpstoxSubscribeRequest;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,6 +16,9 @@ import org.springframework.web.client.RestClient;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
 // Responsibility of this class - Fetch WSS URL from authorize endpoint + maintains Upstox connection
@@ -22,11 +29,53 @@ public class UpstoxWebSocketClient {
 
     private final RestClient restClient;
     private final ObjectMapper objectMapper;
+    private final MarketDataHandler marketDataHandler;
 
-    public URI getWssURL() {
+    private WebSocket activeWebSocketObject;
+
+    // runs initially when the application starts
+    @PostConstruct
+    public void init() {
+        connectWebsocketToUpstox();
+    }
+
+    public void connectWebsocketToUpstox() {
+        HttpClient httpClient = HttpClient.newBuilder().build();
+        CompletableFuture<WebSocket> webSocketConnectionObject = httpClient.newWebSocketBuilder()
+                .buildAsync(getWssURL(), marketDataHandler);
+        activeWebSocketObject = webSocketConnectionObject.join();
+    }
+
+    public void onSubscribe(List<StockSearchDTO> stockRequestList, String method, String mode) {
+        UpstoxSubscribeRequest upstoxSubscribeRequest = UpstoxSubscribeRequest.builder()
+                .guid(UUID.randomUUID().toString())
+                .method(method)
+                .data(UpstoxSubscribeData.builder()
+                        .mode(mode)
+                        .instrumentKeys(getInstrumentKeyList(stockRequestList))
+                        .build()
+                )
+                .build();
+        try {
+            byte[] jsonString = objectMapper.writeValueAsBytes(upstoxSubscribeRequest);
+            activeWebSocketObject.sendBinary(ByteBuffer.wrap(jsonString), true);
+
+        } catch (JsonProcessingException e) {
+            throw new UpstoxFeedException("Failed to serialize subscribe request: " + e.getMessage());
+        }
+    }
+
+    private List<String> getInstrumentKeyList(List<StockSearchDTO> stockRequestList) {
+        return stockRequestList.stream()
+                .map((eachItem) -> eachItem.getInstrumentKey())
+                .toList();
+    }
+
+
+    private URI getWssURL() {
         try {
             String response = restClient.get()
-                    .uri("/feed/market-data-feed/authorize")
+                    .uri("v3/feed/market-data-feed/authorize")
                     .retrieve()
                     .body(String.class);
             JsonNode responseNode = objectMapper.readTree(response);
@@ -41,10 +90,7 @@ public class UpstoxWebSocketClient {
         }
     }
 
-    public WebSocket connectWebsocketToUpstox() {
-        HttpClient httpClient = HttpClient.newBuilder().build();
-        CompletableFuture<WebSocket> webSocketConnectionObject = httpClient.newWebSocketBuilder()
-                .buildAsync(getWssURL(), new MarketDataHandler());
-        return webSocketConnectionObject.join();
+    public WebSocket getActiveWebsocket() {
+        return activeWebSocketObject;
     }
 }
