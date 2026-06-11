87f090ba8d4b2a376000f2a2231f1ea4eb1cd83d
anshs-12 <anshsugandhi12@gmail.com>
2026-06-12 04:17:00 +0530

    feat: create DTOs and subscription payloads for Upstox websocket streams

--- src/main/java/com/stockChecker/live_stock_checker/payload/UpstoxPayload/UpstoxSubscribeData.java
+++ src/main/java/com/stockChecker/live_stock_checker/payload/UpstoxPayload/UpstoxSubscribeData.java
@@ -1,6 +1,7 @@
 package com.stockChecker.live_stock_checker.payload.UpstoxPayload;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 
 public class UpstoxSubscribeData {
     String mode;
--- src/main/java/com/stockChecker/live_stock_checker/payload/UpstoxPayload/UpstoxSubscribeRequest.java
+++ src/main/java/com/stockChecker/live_stock_checker/payload/UpstoxPayload/UpstoxSubscribeRequest.java
@@ -1,12 +1,15 @@
 package com.stockChecker.live_stock_checker.payload.UpstoxPayload;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
+
 public class UpstoxSubscribeRequest {
 
     String guid;
