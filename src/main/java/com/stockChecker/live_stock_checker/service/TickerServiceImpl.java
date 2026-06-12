dba6ac5974651ab3284d99f4353e91016b0b6a96
anshs-12 <anshsugandhi12@gmail.com>
2026-06-13 01:09:13 +0530

    feat: add missing key detection and auto-subscribe in ticker service

--- src/main/java/com/stockChecker/live_stock_checker/service/TickerServiceImpl.java
+++ src/main/java/com/stockChecker/live_stock_checker/service/TickerServiceImpl.java
@@ -1,11 +1,14 @@
 package com.stockChecker.live_stock_checker.service;
 
+import com.stockChecker.live_stock_checker.exceptions.UpstoxFeedException;
 import com.stockChecker.live_stock_checker.payload.WebsocketPayload.FullFeedDataDTO;
 import com.stockChecker.live_stock_checker.payload.WebsocketPayload.LtpcDataDTO;
+import com.stockChecker.live_stock_checker.websocket.UpstoxWebSocketClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,24 +18,49 @@ import java.util.Map;
 public class TickerServiceImpl implements TickerService {
 
     private final RedisTemplate<String, Object> redisTemplate;
+    private final UpstoxWebSocketClient upstoxWebSocketClient;
 
     @Override
     public Map<String, LtpcDataDTO> getLiveLtpcData(List<String> instrumentKeyList) {
         Map<String, LtpcDataDTO> responseMap = new HashMap<>();
-        for (var key : instrumentKeyList) {
-            LtpcDataDTO value = (LtpcDataDTO) redisTemplate.opsForValue().get("LTPC:" + key);
-            responseMap.put(key, value);
+        List<String> missingKeys = new ArrayList<>();
+        List<Object> responseValues = redisTemplate.opsForValue().multiGet(getRedisSyntaxKeys(instrumentKeyList, "LTPC:"));
+        if (instrumentKeyList.size() != responseValues.size()) {
+            throw new UpstoxFeedException("Redis multiGet returned unexpected size");
         }
+        for (int i = 0; i < instrumentKeyList.size(); i++) {
+            if (responseValues.get(i) == null) {
+                missingKeys.add(instrumentKeyList.get(i));
+            }
+            responseMap.put(instrumentKeyList.get(i), (LtpcDataDTO) responseValues.get(i));
+        }
+        if (!missingKeys.isEmpty())
+            upstoxWebSocketClient.onSubscribe(missingKeys, "sub", "ltpc");
         return responseMap;
     }
 
     @Override
     public Map<String, FullFeedDataDTO> getLiveFullFeedData(List<String> instrumentKeyList) {
         Map<String, FullFeedDataDTO> responseMap = new HashMap<>();
-        for (var key : instrumentKeyList) {
-            FullFeedDataDTO value = (FullFeedDataDTO) redisTemplate.opsForValue().get("FULL:" + key);
-            responseMap.put(key, value);
+        List<String> missingKeys = new ArrayList<>();
+        List<Object> responseValues = redisTemplate.opsForValue().multiGet(getRedisSyntaxKeys(instrumentKeyList, "FULL:"));
+        if (instrumentKeyList.size() != responseValues.size()) {
+            throw new UpstoxFeedException("Redis multiGet returned unexpected size");
         }
+        for (int i = 0; i < instrumentKeyList.size(); i++) {
+            if (responseValues.get(i) == null) {
+                missingKeys.add(instrumentKeyList.get(i));
+            }
+            responseMap.put(instrumentKeyList.get(i), (FullFeedDataDTO) responseValues.get(i));
+        }
+        if (!missingKeys.isEmpty())
+            upstoxWebSocketClient.onSubscribe(missingKeys, "sub", "full");
         return responseMap;
+    }
+
+    private List<String> getRedisSyntaxKeys(List<String> instrumentKeyList, String mode) {
+        return instrumentKeyList.stream()
+                .map((eachKey) -> (mode + eachKey))
+                .toList();
     }
 }
